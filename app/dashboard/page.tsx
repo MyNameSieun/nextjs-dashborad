267108583ad638621ef5bae9f6022608f9b43f39
박시은 <charm516@naver.com>
2024-09-22 21:11:59 +0900

    [feat] - Chap 1. Getting Started ~ Chap 5. Navigating Between Pages

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <p>대시보드 페이지</p>;
+};
+
+export default page;
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,22 @@
-import AcmeLogo from '@/app/ui/acme-logo';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import AcmeLogo from "@/app/ui/acme-logo";
+import Link from "next/link";
+import { lusitana } from "./ui/fonts";
+import Image from "next/image";
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
+        <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+          <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" />
+          <p
+            className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
+          >
+            <strong>Welcome to Acme.</strong> This is the example for the{" "}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
@@ -26,6 +31,20 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="데스크톱 및 모바일 버전을 보여주는 대시보드 프로젝트의 스크린샷"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="hidden md:block"
+            alt="모바일 버전을 보여주는 대시보드 프로젝트의 스크린샷"
+          />
         </div>
       </div>
     </main>
